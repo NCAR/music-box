a0ffe25df1f3df4ef670dd30a456f4e1eff8d53f
Matthew Dawson <mattdawson@ucar.edu>
2020-07-24 08:28:15 -0700

    add license boilerplate

--- src/util/io.F90
+++ src/util/io.F90
@@ -1,6 +1,10 @@
 ! Portions copyright (C) 2005-2016 Nicole Riemer and Matthew West
 ! Licensed under the GNU General Public License version 2 or (at your
 ! option) any later version. See the file COPYING for details.
+!
+! Portions Copyright (C) 2020 National Center for Atmospheric Research
+! SPDX-License-Identifier: Apache-2.0
+!
 !> \file
 !> The musica_io module
 
