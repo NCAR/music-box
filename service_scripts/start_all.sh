f63af3787a30a746d0ca1f397d51349bf24900cd
jwoody02 <jowo4949@colorado.edu>
2023-02-20 20:57:03 -0700

    added model runner dockerfile

--- service_scripts/start_all.sh
+++ service_scripts/start_all.sh
@@ -1,6 +1,17 @@
-FROM fedora:33
-
-RUN dnf -y update \
+# check if running as root
+if [ "$(id -u)" != "0" ]; then
+    echo -e "\033[31m This script must be run as root \033[0m" 1>&2
+    exit 1
+fi
+# ask if we want to install the dependencies too (for the first time)
+echo -e "\033[32m Do you want to install the dependencies too? (y/n) \033[0m"
+read -r answer
+if [ "$answer" = "y" ]; then
+    cd ..
+    # install dependencies
+    echo -e "\033[32m installing dependencies \033[0m"
+    echo -e "\033[32m   |_ installing dnf dependencies \033[0m"
+    dnf -y update > /dev/null \
     && dnf -y install \
         gcc-gfortran \
         gcc-c++ \
@@ -24,46 +35,38 @@ RUN dnf -y update \
         nodejs \
         ncview \
 	libpq-devel \
-    && dnf clean all
-
-# python modules needed in scripts
-RUN dnf -y install python3-pandas
-
-RUN pip3 install requests numpy scipy matplotlib ipython jupyter nose Django pillow \
-                 django-crispy-forms pyvis django-cors-headers drf-yasg psycopg2-binary pika
-# Build the SuiteSparse libraries for sparse matrix support
-RUN curl -LO http://faculty.cse.tamu.edu/davis/SuiteSparse/SuiteSparse-5.1.0.tar.gz \
+    && dnf clean all > /dev/null
+    dnf -y install python3-pandas > /dev/null
+    echo -e "\033[32m   |_ installing pip dependencies \033[0m"
+    pip3 install requests numpy scipy matplotlib ipython jupyter nose Django pillow \
+                 django-crispy-forms pyvis django-cors-headers drf-yasg psycopg2-binary pika > /dev/null
+    echo -e "\033[32m   |_ installing SuiteSparse \033[0m"
+    curl -LO http://faculty.cse.tamu.edu/davis/SuiteSparse/SuiteSparse-5.1.0.tar.gz \
     && tar -zxvf SuiteSparse-5.1.0.tar.gz \
     && export CXX=/usr/bin/cc \
     && cd SuiteSparse \
-    && make install INSTALL=/usr/local BLAS="-L/lib64 -lopenblas"
-
-# install json-fortran
-RUN curl -LO https://github.com/jacobwilliams/json-fortran/archive/8.2.0.tar.gz \
+    && make install INSTALL=/usr/local BLAS="-L/lib64 -lopenblas" > /dev/null \
+    echo -e "\033[32m   |_ installing json-fortran \033[0m"
+    curl -LO https://github.com/jacobwilliams/json-fortran/archive/8.2.0.tar.gz \
     && tar -zxvf 8.2.0.tar.gz \
     && cd json-fortran-8.2.0 \
     && export FC=gfortran \
     && mkdir build \
     && cd build \
     && cmake -D SKIP_DOC_GEN:BOOL=TRUE .. \
-    && make install
+    && make install > /dev/null \
 
-# copy the MusicBox code
-COPY . /music-box/
-
-# move the change mechanism script to the root folder
-RUN cp /music-box/etc/change_mechanism.sh /
-
-# move the example configurations to the build folder
-RUN mkdir /build \
+    echo -e "\033[32m   |_ copying music box \033[0m"
+    # TODO COPY . /music-box/
+    
+    echo -e "\033[32m   |_ copy mechanism \033[0m"
+    cp /music-box/etc/change_mechanism.sh /
+    mkdir /build \
     && cp -r /music-box/examples /build/examples
-
-# nodejs modules needed Mechanism-To-Code
-RUN cd /music-box/libs/micm-preprocessor; \
+    cd /music-box/libs/micm-preprocessor; \
     npm install
-
-# Install a modified version of CVODE
-RUN tar -zxvf /music-box/libs/camp/cvode-3.4-alpha.tar.gz \
+    echo -e "\033[32m   |_ installing modified CVODE \033[0m"
+    tar -zxvf /music-box/libs/camp/cvode-3.4-alpha.tar.gz \
     && cd cvode-3.4-alpha \
     && mkdir build \
     && cd build \
@@ -77,9 +80,8 @@ RUN tar -zxvf /music-box/libs/camp/cvode-3.4-alpha.tar.gz \
              -D KLU_INCLUDE_DIR=/usr/local/include \
              .. \
     && make install
-
-# Build CAMP
-RUN mkdir camp_build \
+    echo -e "\033[32m   |_ installing camp \033[0m"
+    mkdir camp_build \
     && cd camp_build \
     && export JSON_FORTRAN_HOME="/usr/local/jsonfortran-gnu-8.2.0" \
     && cmake -D CMAKE_BUILD_TYPE=release \
@@ -89,12 +91,8 @@ RUN mkdir camp_build \
              -D ENABLE_GSL:BOOL=TRUE \
              /music-box/libs/camp \
     && make
-
-# command line arguments
-ARG TAG_ID=false
-
-# get a MICM mechanism if one has been specified
-RUN if [ "$TAG_ID" = "false" ] ; then \
+    TAG_ID=false
+    if [ "$TAG_ID" = "false" ] ; then \
       echo "No mechanism specified" ; else \
       echo "Grabbing mechanism $TAG_ID" \
       && cd /music-box/libs/micm-preprocessor \
@@ -109,26 +107,25 @@ RUN if [ "$TAG_ID" = "false" ] ; then \
         && exit 1 \
         ; fi \
       ; fi
-
-# build the model
-RUN cd /build \
+    echo -e "\033[32m   |_ building music box \033[0m"
+    cd /build \
       && export JSON_FORTRAN_HOME="/usr/local/jsonfortran-gnu-8.2.0" \
       && cmake -D CAMP_INCLUDE_DIR="/camp_build/include" \
                -D CAMP_LIB="/camp_build/lib/libcamp.a" \
                /music-box \
       && make
+    pip3 install django-extensions Werkzeug pyOpenSSL > /dev/null
+fi
+# start rabbit mq
+echo -e "\033[32m starting rabbitmq \033[0m"
+# run 'sudo systemctl start rabbitmq-server' and don't show output
+sudo systemctl start rabbitmq-server > /dev/null
 
 # Prepare the music-box-interactive web server
-RUN mv music-box/libs/music-box-interactive .
-ENV MUSIC_BOX_BUILD_DIR=/build
-
-EXPOSE 8000
-WORKDIR music-box-interactive
-RUN pip3 install django-extensions Werkzeug pyOpenSSL
-# migrate so database works
-RUN python3 interactive/manage.py makemigrations
-RUN python3 interactive/manage.py migrate
-
-# Start stuff
-CMD sh start_servers.sh
-#CMD ["python3", "music-box-interactive/interactive/manage.py", "runserver", "0.0.0.0:8000" ]
+echo -e "\033[32m preparing music-box-interactive web server \033[0m"
+mv music-box/libs/music-box-interactive .
+MUSIC_BOX_BUILD_DIR=/build
+python3 libs/interactive/manage.py makemigrations > /dev/null
+python3 libs/interactive/manage.py migrate
+sh start_servers.sh &
+echo -e "\033[32m Done! everything should be running in background \033[0m"
