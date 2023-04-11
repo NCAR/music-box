2eace18bac3e3c36ba75ac4d00d803156717e01f
Kyle Shores <kyle.shores44@gmail.com>
2023-04-11 17:45:44 -0500

    preserving the hyrdogen? ions

--- capram_convert/convert.py
+++ capram_convert/convert.py
@@ -41,8 +41,28 @@ diss = []
 
 # pattern that matches A, B, and optionall C constants for PHOTABC and TEMP3 reactions
 # thanks chatgpt
-pattern = re.compile(r"(PHOTABC|TEMP3|DCONST|DTEMP):\s+A:\s+([\d\.e+-]+)\s+B:\s+([\d\.e+-]+)(?:\s+C:\s+([\d\.e+-]+))?")
+constants_pattern = re.compile(r"(PHOTABC|TEMP3|DCONST|DTEMP):\s+A:\s+([\d\.e+-]+)\s+B:\s+([\d\.e+-]+)(?:\s+C:\s+([\d\.e+-]+))?")
+reaction_pattern = r"^([^=]*)=([^=]*)$"
 
+def parse_reactants_products(line):
+    match = re.search(reaction_pattern, line.replace(" ", ""))
+    if match:
+        reactants = combine_stoichiometric_coeffs(match.group(1).strip())
+        products = combine_stoichiometric_coeffs(match.group(2).strip())
+    return reactants, products
+
+def combine_stoichiometric_coeffs(species):
+    # given this: O2m   +  FEpp = FEppp  + aH2O2  -2.0 Hp 
+    # output this for the products: ['FEppp', 'aH2O2', '-2.0Hp']
+    groups = [i for i in re.split(r'([+-])', species) if i != '+']
+    result = []
+    for i, group in enumerate(groups):
+        if (i > 0) and (groups[i-1] == '-'):
+            result.append(groups[i-1] + group)
+        elif group != '-':
+            result.append(group)
+
+    return result
 
 line_index = 0
 while line_index < len(content):
@@ -64,25 +84,21 @@ while line_index < len(content):
 
             line_index += 2
         elif type == 'AQUA':
-            reaction = content[line_index + 1].strip().split('=')
-            reactants = [i.strip() for i in reaction[0].strip().split('+')]
-            products = [i.strip() for i in reaction[1].strip().split('+')]
-            # thanks chatgpt
-            match = pattern.search(content[line_index + 2])
-            if content[line_index + 2].startswith('PHOTABC'):
-                A, B, C = match.group(2), match.group(3), match.group(4)
+            reactants, products = parse_reactants_products(content[line_index + 1])
+            match = constants_pattern.search(content[line_index + 2])
+            type, A, B, C = match.group(1), match.group(2), match.group(3), match.group(4)
+            if type == 'PHOTABC':
                 aqua_photo.append(dict(reactants = reactants, products=products, A=A, B=B, C=C))
-            elif content[line_index + 2].startswith('TEMP3'):
-                A, B = match.group(2), match.group(3)
+            elif type == 'TEMP3':
                 aqua_temp.append(dict(reactants = reactants, products=products, A=A, B=B))
             else:
-                print(f"Unknown aqua type: {content[line_index + 2]}")
+                print(f"Unknown aqua type: {type}")
             line_index += 2
         elif type == 'DISS':
             reaction = content[line_index + 1].strip().split('=')
             reactants = [i.strip() for i in reaction[0].strip().split('+')]
             products = [i.strip() for i in reaction[1].strip().split('+')]
-            match = pattern.search(content[line_index + 2])
+            match = constants_pattern.search(content[line_index + 2])
             type, A, B, C = match.group(1), match.group(2), match.group(3), match.group(4)
             if C is None:
                 diss.append(dict(reactants = reactants, products=products, A=A, B=B))
@@ -91,3 +107,8 @@ while line_index < len(content):
         else:
             print(f'Unknown type: {type}')
     line_index += 1
+
+
+print(aqua_photo)
+print()
+print(aqua_temp)
