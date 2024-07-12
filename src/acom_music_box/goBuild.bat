1ca853f9f92ed88c67d174298f2c64c7707ff828
Carl Drews <drews@ucar.edu>
2024-07-12 16:03:01 -0600

    Removed helper scripts from repository.

--- src/acom_music_box/goBuild.bat
+++
@@ -1,3 +0,0 @@
-cd ..\..
-pip install .
-cd src\acom_music_box
--- src/acom_music_box/goMusicBox.bat
+++
@@ -1,1 +0,0 @@
-python music_box_main.py configFile="C:\2024\MusicBox\music-box\tests\configs\analytical_config\my_config.json" outputDir="C:\2024\MusicBox\music-box\tests\configs\analytical_config"
