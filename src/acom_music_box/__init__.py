59f893a87f91cdcf640512b907a6e01d6136acbd
Kyle Shores <kyle.shores44@gmail.com>
2024-08-09 06:54:01 -0500

    adding both files to cache

--- src/acom_music_box/__init__.py
+++ src/acom_music_box/__init__.py
@@ -4,7 +4,7 @@ This is the music_box package.
 This package contains modules for handling various aspects of a music box, 
 including species, products, reactants, reactions, and more.
 """
-__version__ = "2.1.4"
+__version__ = "2.1.5"
 
 from .utils import convert_time, convert_pressure, convert_temperature, convert_concentration
 from .music_box_species import Species
