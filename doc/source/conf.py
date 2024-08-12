cb85470d250214805e5243e5a04215b45b010fae
Kyle Shores <kyle.shores44@gmail.com>
2024-08-12 09:31:42 -0500

    setting up music box docs with our theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,6 +1,8 @@
 import sys
 import os
+import datetime
 sys.path.insert(0, os.path.abspath('..'))
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -10,13 +12,17 @@ sys.path.insert(0, os.path.abspath('..'))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'acom_music_box'
-copyright = '2024, NCAR'
-author = 'NCAR'
+copyright = f'2024-{datetime.datetime.now().year}, NCAR/UCAR'
+author = 'NCAR/UCAR'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx_copybutton',
+    'sphinx_design',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -26,5 +32,13 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
 html_static_path = ['_static']
+html_theme = 'pydata_sphinx_theme'
+
+html_theme_options = {
+    "external_links": [],
+    "github_url": "https://github.com/NCAR/music-box",
+    "navbar_end": ["version-switcher", "navbar-icon-links"],
+    "pygment_light_style": "tango",
+    "pygment_dark_style": "monokai"
+}
