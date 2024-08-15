06571f002739c392fcebb1f841c6551f612cb6c9
Mudassir Chapra <37051110+muddi900@users.noreply.github.com>
2024-08-15 18:11:54 +0500

    Remove all redundant `music_box_` from file names and updated references [Issue #177] (#180)
    * Remove all redundant `music_box_` from file names and updated references [Issue #177]
    
    * correcting relative imports
    
    * correcting more imports
    
    * renaming executable
    
    ---------
    
    Co-authored-by: Kyle Shores <kyle.shores44@gmail.com>

--- src/acom_music_box/__init__.py
+++ src/acom_music_box/__init__.py
@@ -7,15 +7,15 @@ including species, products, reactants, reactions, and more.
 __version__ = "2.1.5"
 
 from .utils import convert_time, convert_pressure, convert_temperature, convert_concentration
-from .music_box_species import Species
-from .music_box_product import Product
-from .music_box_reactant import Reactant
-from .music_box_reaction import Reaction, Branched, Arrhenius, Tunneling, Troe_Ternary
-from .music_box_species_list import SpeciesList
-from .music_box_model_options import BoxModelOptions
-from .music_box_species_concentration import SpeciesConcentration
-from .music_box_reaction_rate import ReactionRate
-from .music_box_conditions import Conditions
+from .species import Species
+from .product import Product
+from .reactant import Reactant
+from .reaction import Reaction, Branched, Arrhenius, Tunneling, Troe_Ternary
+from .species_list import SpeciesList
+from .model_options import BoxModelOptions
+from .species_concentration import SpeciesConcentration
+from .reaction_rate import ReactionRate
+from .conditions import Conditions
 
-from .music_box_evolving_conditions import EvolvingConditions
+from .evolving_conditions import EvolvingConditions
 from .music_box import MusicBox
