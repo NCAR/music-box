73c465b83888d68ce50de357377bdec983e1866b
carl-drews <drews@modeling2.acom.ucar.edu>
2024-09-16 08:54:32 -0600

    Added the Pass test config.

--- tests/integration/test_duplicate_reactions.py
+++ tests/integration/test_duplicate_reactions.py
@@ -1,4 +1,5 @@
-from acom_music_box import MusicBox, Examples
+from acom_music_box import MusicBox, Reaction, ReactionRate, Conditions
+from acom_music_box.reaction_list import ReactionList
 
 import os
 import csv
@@ -7,7 +8,16 @@ import math
 
 class TestDuplicateReactions:
     def test_run(self):
-        box_model = MusicBox()
+        # set up dummy reactions
+        abc123 = ReactionRate(Reaction("abc"), 12.3)
+        def456 = ReactionRate(Reaction("def"), 45.6)
+        abc789 = ReactionRate(Reaction("abc"), 78.9)
+
+        # Pass: unique reaction names
+        pass_reactions = ReactionList(name="Pass list", reactions=[abc123, def456])
+        pass_conditions = Conditions(reaction_rates=pass_reactions)
+        box_model = MusicBox(initial_conditions=pass_conditions)
+        box_model.check_config("Loaded from string.")
 
         assert True, f"All is good."
 
