62863036ca26f83e1b5712b9a15adc80326920b0
Carl Drews <drews@ucar.edu>
2024-09-24 11:16:03 -0600

    Detect and display error for duplicate reaction names (keys) - Issue #235 (#241)
    * Adding check for duplicate MUSICA keys.
    
    * Removed diagnosic code.
    
    * Added stub for unit test.
    
    * Added the Pass test config.
    
    * Finalized the test for duplicate reaction names.
    
    * Moved from integration tests to unit tests.
    
    * Use pytest to verify exception raised.
    
    * Removed unneeded imports.
    
    * Simpler assert check for success.
    
    * Moved the configuration check to within readConditionsFromJson(), and at the end.
    
    * Fixed indentation mistake.
    
    ---------
    
    Co-authored-by: carl-drews <drews@modeling2.acom.ucar.edu>

--- src/acom_music_box/music_box.py
+++ src/acom_music_box/music_box.py
@@ -69,6 +69,67 @@ class MusicBox:
             time=[time_point], conditions=[conditions])
         self.evolvingConditions.append(evolving_condition)
 
+    def create_solver(
+        self,
+        path_to_config,
+        solver_type=musica.micmsolver.rosenbrock,
+        number_of_grid_cells=1):
+        """
+        Creates a micm solver object using the CAMP configuration files.
+
+        Args:
+            path_to_config (str): The path to CAMP configuration directory.
+
+        Returns:
+            None
+        """
+        # Create a solver object using the configuration file
+        self.solver = musica.create_solver(
+            path_to_config,
+            solver_type,
+            number_of_grid_cells)
+
+    def check_config(self, boxConfigPath):
+        """
+        Verifies correct configuration of the MusicBox object.
+        There is intentionally no check for the presence of a solver;
+        this test function is for the loaded configuration only.
+
+        Args:
+            boxConfigPath = filename and path of MusicBox configuration file
+                This filename is supplied only for the error message;
+                the configuration should already be loaded.
+
+        Returns:
+            True if all checks passed
+            Throws error for the first check failed.
+        """
+
+        # look for duplicate reaction names
+        if (self.initial_conditions):
+            if (self.initial_conditions.reaction_rates):
+                reactionNames = []
+                for rate in self.initial_conditions.reaction_rates:
+                    # watch out for Nones in here
+                    if not rate.reaction:
+                        continue
+                    if not rate.reaction.name:
+                        continue
+                    reactionNames.append(rate.reaction.name)
+
+                # look for name already seen
+                seen = set()
+                dupNames = [name for name in reactionNames if name in seen or seen.add(name)]
+
+                if (len(dupNames) > 0):
+                    # inform user of the error and its remedy
+                    errString = ("Error: Duplicate reaction names specified within {}: {}."
+                            .format(boxConfigPath, dupNames))
+                    errString += " Please remove or rename the duplicates."
+                    raise Exception(errString)
+
+        return(True)
+
     def solve(self, output_path=None, callback=None):
         """
         Solves the box model simulation and optionally writes the output to a file.
@@ -259,6 +320,8 @@ class MusicBox:
             # Set initial conditions
             self.evolving_conditions = EvolvingConditions.from_config_JSON(
                 path_to_json, data, self.species_list, self.reaction_list)
+
+        self.check_config(os.path.join(os.getcwd(), path_to_json))
 
         camp_path = os.path.join(
             os.path.dirname(path_to_json),
