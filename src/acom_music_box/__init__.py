7cf5269defc08a123139f27edf44245d7b9527cb
Kyle Shores <kyle.shores44@gmail.com>
2024-10-03 10:19:32 -0500

    using newest musica version

--- src/acom_music_box/__init__.py
+++ src/acom_music_box/__init__.py
@@ -1,10 +1,10 @@
 """
-This is the music_box package.
+An atmospheric chemistry box model. Powered by MUSICA.
 
 This package contains modules for handling various aspects of a music box,
 including species, products, reactants, reactions, and more.
 """
-__version__ = "2.5.0"
+__version__ = "2.5.1"
 
 from .utils import convert_time, convert_pressure, convert_temperature, convert_concentration
 from .species import Species
