80c2e0247f5cf1c1f24fffe6061edccb1f282339
K20shores <K20shores@users.noreply.github.com>
2024-10-09 20:22:13 +0000

    deploy: 3238d4c687f2a22ac5ded164fbcee5c808eaf63c

---
+++ tests/integration/input_use_cases/4/run_b.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# exit on error
+set -e
+# turn on command echoing
+set -v
+# make sure that the current directory is the once where this script is
+cd ${0%/*}
+
+exec_str="../../../../music_box config_b.json"
+comp_str="../../../../compare_results output.csv expected_output.csv 1.0e-3 1.0e-12"
+
+if ! $exec_str; then
+  echo FAIL
+  exit 1
+else
+  if $comp_str; then
+    echo PASS
+    exit 0
+  else
+    echo unexpected results
+    echo FAIL
+    exit 1
+  fi
+fi
