b9499d8830de3861536ae26da262e4f282fdc04f
Kyle Shores <kyle.shores44@gmail.com>
2025-07-03 08:31:59 -0500

    captured all reactions

--- src/acom_music_box/tools/geckoAToMusicBox.py
+++ src/acom_music_box/tools/geckoAToMusicBox.py
@@ -5,14 +5,22 @@ import os
 import pandas as pd
 import re
 
+
 def parse_arguements():
-    parser = argparse.ArgumentParser(description='GECKO-A to MusicBox Conversion tool.')
-    parser.add_argument('-i', '--input', type=str, help='Path to a directory containing GECKO-A configuration files.')
-    parser.add_argument('-o', '--output', type=str, help='Path to save the output file, including the file name. If not provided, result will be printed to the console.')
-    parser.add_argument('-v', '--verbose', action='count', default=0, help='Increase logging verbosity. Use -v for info, -vv for debug.')
-    parser.add_argument('--color-output', action='store_true', help='Enable color output for logs.')
-    parser.add_argument('--version', action='version', version='MusicBox 0.1.0')
+    parser = argparse.ArgumentParser(
+        description='GECKO-A to MusicBox Conversion tool.')
+    parser.add_argument('-i', '--input', type=str,
+                        help='Path to a directory containing GECKO-A configuration files.')
+    parser.add_argument('-o', '--output', type=str,
+                        help='Path to save the output file, including the file name. If not provided, defaults to mechanism.json.')
+    parser.add_argument('-v', '--verbose', action='count', default=0,
+                        help='Increase logging verbosity. Use -v for info, -vv for debug.')
+    parser.add_argument('--color-output', action='store_true',
+                        help='Enable color output for logs.')
+    parser.add_argument('--version', action='version',
+                        version='MusicBox 0.1.0')
     return parser.parse_args()
+
 
 def setup_logging(verbosity, color_output):
     log_level = logging.DEBUG if verbosity >= 2 else logging.INFO if verbosity == 1 else logging.CRITICAL
@@ -38,10 +46,11 @@ def setup_logging(verbosity, color_output):
     console_handler.setLevel(log_level)
     logging.basicConfig(level=log_level, handlers=[console_handler])
 
+
 def parse_species(input_path, logger):
     # this defines the species in the system and some properties they have
     path = os.path.join(input_path, 'dictionary.out')
-    
+
     df = pd.read_fwf(
         path,
         skiprows=1,  # Skip the first line with metadata
@@ -54,14 +63,14 @@ def parse_species(input_path, logger):
     # drop the last row reprsenting the end
     df = df.drop(df.tail(1).index)
 
-    dtypes={
-      "GECKO-A Name": str,
-      "Structural Format": str,
-      "Short Code": str,
-      "Molar Mass": float,
-      "Stability Flag": int,
-      "C": int, "H": int, "N": int, "O": int, "S": int,
-      "F": int, "Cl": int, "Br": int
+    dtypes = {
+        "GECKO-A Name": str,
+        "Structural Format": str,
+        "Short Code": str,
+        "Molar Mass": float,
+        "Stability Flag": int,
+        "C": int, "H": int, "N": int, "O": int, "S": int,
+        "F": int, "Cl": int, "Br": int
     }
 
     # set the dtypes
@@ -69,15 +78,16 @@ def parse_species(input_path, logger):
 
     return df
 
+
 def parse_reactions(input_path, logger):
     reactions = []
     path = os.path.join(input_path, 'reactions.dum')
     with open(path, 'r') as file:
         lines = iter(file.readlines())
-    
+
     current_reaction = None
     multi_line_products = None
-    
+
     def parse_species_list(species_str):
         """
         Parse a string of species and coefficients into a list of dictionaries.
@@ -87,32 +97,40 @@ def parse_reactions(input_path, logger):
             part = part.strip()
             if ' ' in part:
                 coefficient, name = part.split(' ', 1)
-                species_list.append({'species name': name.strip(), 'coefficient': float(coefficient)})
+                species_list.append(
+                    {'species name': name.strip(), 'coefficient': float(coefficient)})
             else:
                 # sometimes part can be an emptry string, ignore it
                 if part:
-                    species_list.append({'species name': part, 'coefficient': 1.0})
+                    species_list.append(
+                        {'species name': part, 'coefficient': 1.0})
         return species_list
-    
+
     for line in lines:
         line = line.strip()
-        
+
+        found = line.count("G3K5000 + GNO3 ") > 0
+        if found:
+            logger.warning(
+                "Found a reaction with G2K6001 + PERO9, this is a known issue in GECKO-A. This reaction will be skipped.")
+
         if not line or line.startswith("!"):
             continue  # Skip empty lines and comments
-        
+
         # match a reaction that is all on one line, with arrhenius values
-        match = re.match(r"^(.*?)(?:=>)(.*?)([+\-]?\d\.\d{3}E[+\-]?\d{2}\s+[+\-]?\d+\.\d\s+[+\-]?\d+)", line)
+        match = re.match(
+            r"^(.*?)(?:=>)(.*?)([+\-]?\d\.\d{3}E[+\-]?\d{2}\s+[+\-]?\d+\.\d\s+[+\-]?\d+)", line)
 
         if match:
             if current_reaction:
                 reactions.append(current_reaction)
-            
+
             reactants = match.group(1).strip()
             products = match.group(2).strip()
             reaction_values = match.group(3).strip()
-            
+
             A, n, ER = map(float, reaction_values.split())
-            
+
             current_reaction = {
                 "reactants": parse_species_list(reactants),
                 "products": parse_species_list(products),
@@ -123,7 +141,7 @@ def parse_reactions(input_path, logger):
                 "extra_values": None,
             }
             multi_line_products = None
-        
+
         # special reactions with extra values will be a line with 2 slashes
         elif line.count("/") == 2:
             if current_reaction:
@@ -132,12 +150,20 @@ def parse_reactions(input_path, logger):
                 extra_numbers = line[first_slash + 1:second_slash]
                 extra_values = extra_numbers.split()
                 current_reaction["type"] = line[:first_slash].strip()
-                current_reaction["extra_values"] = [float(val) for val in extra_values]
+                current_reaction["extra_values"] = [
+                    float(val) for val in extra_values]
                 reactions.append(current_reaction)
                 current_reaction = None
-        
+            else:
+                raise RuntimeError(
+                    f"Reaction parsing error: found extra values without a preceding reaction. Line: {line}")
+
         # some reactions span multiple lines, this is the start of that
         elif re.match(r"^(.*?)=>\s*(.*?\+)\s*$", line):
+            if current_reaction:
+                reactions.append(current_reaction)
+                current_reaction = None
+
             match = re.match(r"^(.*?)=>\s*(.*?\+)\s*$", line)
             reactants = match.group(1).strip()
             products = match.group(2).strip()
@@ -158,30 +184,33 @@ def parse_reactions(input_path, logger):
         # this is the continuation of a multi-line reaction
         elif multi_line_products:
             # Regex to detect products and reaction values on the same line
-            match = re.match(r"^(.*?)([+\-]?\d\.\d{3}E[+\-]?\d{2}\s+[+\-]?\d+\.\d\s+[+\-]?\d+)", line)
+            match = re.match(
+                r"^(.*?)([+\-]?\d\.\d{3}E[+\-]?\d{2}\s+[+\-]?\d+\.\d\s+[+\-]?\d+)", line)
             if match:
                 products = match.group(1).strip()
                 reaction_values = match.group(2).strip()
-                
+
                 # Parse the products
-                current_reaction["products"].extend(parse_species_list(products))
-                
+                current_reaction["products"].extend(
+                    parse_species_list(products))
+
                 # Parse the reaction values (A, n, E/R)
                 A, n, ER = map(float, reaction_values.split())
                 current_reaction["A"] = A
                 current_reaction["n"] = n
                 current_reaction["E/R"] = ER
-                
+
                 # Append the completed reaction to the list
                 reactions.append(current_reaction)
-                
+
                 # Reset for the next reaction
                 current_reaction = None
                 multi_line_products = None
             else:
                 # Only products, continue accumulating
                 additional_products = line.strip()
-                current_reaction["products"].extend(parse_species_list(additional_products))
+                current_reaction["products"].extend(
+                    parse_species_list(additional_products))
                 if not additional_products.endswith("+"):
                     multi_line_products = None  # End multi-line tracking
 
@@ -189,49 +218,54 @@ def parse_reactions(input_path, logger):
         else:
             logger.debug(f"Skipping line: {line}")
             continue
-    
+
     if current_reaction:
         reactions.append(current_reaction)
-    
+
     reaction_types = [
         'FALLOFF', 'HV', 'ISOM', 'EXTRA',
     ]
     # remove any reactants whose name is a reaction type
     for r in reactions:
-        r['reactants'] = [s for s in r['reactants'] if s['species name'] not in reaction_types]
-        r['products'] = [s for s in r['products'] if s['species name'] not in reaction_types]
+        r['reactants'] = [s for s in r['reactants']
+                          if s['species name'] not in reaction_types]
+        r['products'] = [s for s in r['products']
+                         if s['species name'] not in reaction_types]
     return reactions
-  
+
+
 def parse_molecular_weights(input_path, logger):
     paths = [
-      ('gasspe.dum', 2),
-      ('partspe.dum', 1)
+        ('gasspe.dum', 2),
+        ('partspe.dum', 1)
     ]
 
     molecular_weights = {}
 
     for path, skip in paths:
-      # read a csv separated by /, skip the first 2 lines
-      df = pd.read_csv(
-          os.path.join(input_path, path),
-          sep='/',
-          skiprows=skip,
-          header=None,
-          names=['Species', 'Molecular Weight', 'Empty'],
-          engine='python'
-      )
+        # read a csv separated by /, skip the first 2 lines
+        df = pd.read_csv(
+            os.path.join(input_path, path),
+            sep='/',
+            skiprows=skip,
+            header=None,
+            names=['Species', 'Molecular Weight', 'Empty'],
+            engine='python'
+        )
 
-      # we don't need the empty column
-      df = df.drop(columns=['Empty'])
+        # we don't need the empty column
+        df = df.drop(columns=['Empty'])
 
-      # drop the last row
-      df = df.drop(df.tail(1).index)
+        # drop the last row
+        df = df.drop(df.tail(1).index)
 
-      df['Species'] = df['Species'].str.strip()
+        df['Species'] = df['Species'].str.strip()
 
-      molecular_weights.update(df.set_index('Species')['Molecular Weight'].to_dict())
+        molecular_weights.update(df.set_index('Species')[
+                                 'Molecular Weight'].to_dict())
 
     return molecular_weights
+
 
 def parse_chemical_formulas(input_path, logger):
     """
@@ -273,20 +307,21 @@ def read_peroxy_species(input_path, logger):
     peroxy_groups = {}
 
     for peroxy in range(1, 10):
-      # the path is always pero#.dat
-      path = os.path.join(input_path, f"pero{peroxy}.dat")
-      with open(path, 'r') as file:
-          lines = file.readlines()
-          n_species = int(lines[0].split()[0])
-          species = []
-          if (n_species > 0):
-              for line in lines[1:]:
-                  species.append(line.strip())
-              # each file ends with END, remove that species
-              del species[-1]
-          peroxy_groups[f"PERO{peroxy}"] = species
-    
+        # the path is always pero#.dat
+        path = os.path.join(input_path, f"pero{peroxy}.dat")
+        with open(path, 'r') as file:
+            lines = file.readlines()
+            n_species = int(lines[0].split()[0])
+            species = []
+            if (n_species > 0):
+                for line in lines[1:]:
+                    species.append(line.strip())
+                # each file ends with END, remove that species
+                del species[-1]
+            peroxy_groups[f"PERO{peroxy}"] = species
+
     return peroxy_groups
+
 
 def convert_reactions_to_musica(reactions, logger):
     """
@@ -397,7 +432,8 @@ def convert_reactions_to_musica(reactions, logger):
                 "products": r['products']
             }
         else:
-            logger.warning(f"Unknown reaction type, skipping conversion. Reaction data: {r}")
+            logger.warning(
+                f"Unknown reaction type, skipping conversion. Reaction data: {r}")
             continue
 
         converted_reactions.append(converted_reaction)
@@ -415,10 +451,10 @@ def main():
     output = args.output
 
     if not input:
-      error = "No input directory provided."
-      logger.error(error)
-      raise ValueError(error)
-    
+        error = "No input directory provided."
+        logger.error(error)
+        raise ValueError(error)
+
     logger.debug(f"Input directory: {input}")
     logger.debug(f"Output file: {output}")
 
@@ -430,10 +466,10 @@ def main():
 
     type_counts = {}
     for r in reactions:
-        name = "_".join([s['name'] for s in r['reactants'] + r['products']])
-        print(name, r['reactants'], r['products'])
+        # name = "_".join([s['name'] for s in r['reactants'] + r['products']])
+        # print(name, r['reactants'], r['products'])
         type_counts[r['type']] = type_counts.get(r['type'], 0) + 1
-    
+
     logger.debug(f"parsed {len(species)} species")
     logger.debug(f"parsed {len(reactions)} reactions")
     logger.debug(f"reaction type counts: {type_counts}")
@@ -446,7 +482,7 @@ def main():
         for s in r['reactants'] + r['products']:
             unique_species.add(s['species name'])
 
-    # Output species to mechanism.json
+    # Output species to mechanism file
     import json
     species_list = []
     for _, row in species.iterrows():
@@ -454,7 +490,8 @@ def main():
         entry = {"name": name}
         mw = molecular_weights.get("G" + name)
         if mw is not None and float(mw) != 1:
-            entry["molecular weight [kg mol-1]"] = float(mw) / 1000.0  # Convert g/mol to kg/mol
+            # Convert g/mol to kg/mol
+            entry["molecular weight [kg mol-1]"] = float(mw) / 1000.0
         formula = chemical_formulas.get(name)
         if formula is not None:
             entry["__chemical formula"] = formula
@@ -468,8 +505,14 @@ def main():
         "species": species_list,
         "reactions": reactions
     }
-    with open('mechanism.json', 'w') as f:
-        json.dump(mechanism_json, f, indent=2)
-    logger.info('GECKO mechanism written to mechanism.json')
 
-    
+    # Use output flag or default to mechanism.json
+    output_file = output if output else 'mechanism.json'
+
+    with open(output_file, 'w') as f:
+        json.dump(mechanism_json, f, indent=2)
+    logger.info(f'GECKO mechanism written to {output_file}')
+
+
+if __name__ == "__main__":
+    main()
