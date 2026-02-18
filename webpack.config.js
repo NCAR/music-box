c1a6baabe440a834f3ddbf96ae7d5e7449a08dbe
Kyle Shores <kyle.shores44@gmail.com>
2026-02-18 10:32:30 -0600

    adding javascript test

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default {
-  entry: './src/index.js',
+  entry: './javascript/src/index.js',
   output: {
     filename: 'music-box.bundle.js',
     path: path.resolve(__dirname, 'dist'),
