b4ce49260c66c52c5376e20a79cd07e10e22964d
carl-drews <drews@modeling2.acom.ucar.edu>
2026-04-26 22:07:05 -0600

    Mimic framework of other main integration tests.

--- python/tests/integration/test_waccm_evolving_conditions.py
+++ python/tests/integration/test_waccm_evolving_conditions.py
@@ -1,59 +1,91 @@
 from acom_music_box import MusicBox
 
 import os
+import pytest
+import tempfile
+import sys
+from acom_music_box.main import main as musicBoxMain
+from acom_music_box.tools.waccmToMusicBox import main as waccmToMusicBoxMain
 import pandas as pd
 import math
 
 
-class TestWaccmEvolvingConditions:
-    def test_run(self):
-
-        # Create a required CSV by converting WACCM data.
-        repo_root = get_repo_root()
-        sample_data_dir = os.path.join(repo_root, "sample_waccm_data")
-
-        # set up the output files
-        csvOutPath = os.path.join(repo_root, "python", "tests", "integration",
-            "configs", "waccm_evolving_conditions", "evolving_conditions.csv")
-
-        # Set up arguments for the WACCM conversion
-        args = [
-            "--waccmDir", f"{sample_data_dir}",
-            "--date", "20260208,20260208",
-            "--time", "00:00,23:00",
-            "--latitude", "2.7,13.8",
-            "--longitude", "101.7,123.8",
-            "--altitude", "567.8,4567.8",
-            "--output", csvOutPath,
-            "--verbose"
-        ]
-
-        # Run the waccmToMusicBox script with the arguments
-        run_waccm_to_music_box_with_args(args, temp_dir)
-
-        # Now run MusicBox with that CSV file containing WACCM data.
-        # This portion of the test will fail if waccmToMusicBox did not run correctly.
-        music_box = MusicBox()
-
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        config_path = os.path.join(current_dir, "configs", "waccm_evolving_conditions", "my_config.json")
-        music_box.loadJson(config_path)
-
-        results = music_box.solve()
-        assert results is not None
-
-        # Get values from DataFrame columns
-        temperatures = results["ENV.temperature.K"]
-        pressures = results["ENV.pressure.Pa"]
-        assert len(temperatures) == len(pressures)
-
-        for temp, press in zip(temperatures, pressures):
-            assert temp is not None
-            assert press is not None
-            assert math.isclose(temp, 287.0, rel_tol=0.004)
-            assert math.isclose(press, 78750.0, rel_tol=0.002)
+@pytest.fixture
+def temp_dir():
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        yield tmpdirname
 
 
-if __name__ == "__main__":
-    test = TestWaccmEvolvingConditions()
-    test.test_run()
+def get_repo_root():
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.."))
+
+
+# cmdName = "waccmToMusicBox" or "music_box"
+# args = command-line parameters
+# cmd = temporary directory for running this test
+def run_command_with_args(cmdName, args, cwd):
+    original_argv = sys.argv
+    original_cwd = os.getcwd()
+    sys.argv = [cmdName] + args
+    try:
+        os.chdir(cwd)
+        if (cmdName == "waccmToMusicBox"):
+            waccmToMusicBoxMain()
+        if (cmdName == "music_box"):
+            musicBoxMain()
+    finally:
+        os.chdir(original_cwd)
+        sys.argv = original_argv
+
+
+def test_waccm_evolving_conditions(temp_dir):
+    print(f"temp_dir1 = {temp_dir}")
+
+    # Step 1: Create a required CSV by converting WACCM data.
+    repo_root = get_repo_root()
+    sample_data_dir = os.path.join(repo_root, "sample_waccm_data")
+
+    # set up the output files
+    configPath = os.path.join(repo_root, "python", "tests", "integration",
+        "configs", "waccm_evolving_conditions")
+    csvOutPath = os.path.join(repo_root, "python", "tests", "integration",
+        "configs", "waccm_evolving_conditions", "evolving_conditions.csv")
+
+    # Set up arguments for the WACCM conversion
+    args = [
+        "--waccmDir", f"{sample_data_dir}",
+        "--date", "20260208,20260208",
+        "--time", "00:00,23:00",
+        "--latitude", "2.7,13.8",
+        "--longitude", "101.7,123.8",
+        "--altitude", "567.8,4567.8",
+        "--template", configPath,
+        "--output", csvOutPath,
+        "--verbose"
+    ]
+
+    # Run the waccmToMusicBox script with the arguments
+    run_command_with_args("waccmToMusicBox", args, temp_dir)
+    assert os.path.exists(os.path.join(temp_dir, csvOutPath))
+
+    # Step 2: Now run MusicBox with that CSV file containing WACCM data.
+    # This portion of the test will fail if waccmToMusicBox did not run correctly.
+
+    # capture the output file
+    configPath = os.path.join(repo_root, "python", "tests", "integration",
+        "configs", "waccm_evolving_conditions", "my_config.json")
+    outputPath = os.path.join(repo_root, "python", "tests", "integration",
+        "configs", "waccm_evolving_conditions", "evolving_output.csv")
+
+    # Set up arguments for the WACCM conversion
+    args = [
+        "--config", configPath,
+        "--output", outputPath,
+        "--verbose", "-v"
+    ]
+
+    # Run the waccmToMusicBox script with the arguments
+    run_command_with_args("music_box", args, temp_dir)
+    print(f"outputPath = {outputPath}")
+    assert os.path.exists(os.path.join(temp_dir, outputPath))
+
