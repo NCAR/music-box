f6f2dcc83c324f949ba1b4ab63473387dba352c9
Carl Drews <drews@ucar.edu>
2026-06-05 09:21:29 -0600

    469 species mapping (#478)
    * modelToMusicBox.py
    
    * Move mapping into a separate file.
    
    * Removed extraneous imports.
    
    ---------
    
    Co-authored-by: carl-drews <drews@modeling2.acom.ucar.edu>

--- python/acom_music_box/tools/modelToMusicBox.py
+++ python/acom_music_box/tools/modelToMusicBox.py
@@ -6,7 +6,6 @@
 # Author: Carl Drews
 # Copyright 2024 by Atmospheric Chemistry Observations & Modeling (UCAR/ACOM)
 
-# import os
 import pathvalidate
 import argparse
 import datetime
@@ -22,6 +21,7 @@ from acom_music_box.utils import calculate_air_density
 import netCDF4
 from acom_music_box.tools import gridUtils
 from acom_music_box.tools import fileUtils
+from acom_music_box.tools import speciesMap
 from acom_music_box import conditions_manager
 import copy
 
@@ -210,64 +210,6 @@ def getMusicaSpecies(myConfigFile):
             musicaNames.append(spec.get("name"))
 
     return (musicaNames)
-
-
-# Build and return dictionary of WACCM variable names
-# and their MusicBox equivalents.
-# waccmSpecies = list of variable names in the WACCM model output
-# musicaSpecies = list of variable names in species.json
-# return ordered dictionary
-def getMusicaDictionary(modelType, waccmSpecies=None, musicaSpecies=None):
-    if ((waccmSpecies is None) or (musicaSpecies is None)):
-        logger.warning("No species map found for WACCM or MUSICA.")
-
-        # build a simple species map
-        varMap = {
-            "T": "temperature",
-            "lev": "pressure",      # WACCM sigma pressure coordinates
-            "N2O": "N2O",
-            "H2O2": "H2O2",
-            "O3": "O3",
-            "NH3": "NH3",
-            "CH4": "CH4"
-        }
-
-        return (dict(sorted(varMap.items())))
-
-    # create new list of species common to both lists
-    inCommon = sorted([species for species in waccmSpecies if species in musicaSpecies])
-
-    # provide some diagnostic warnings
-    # If these messages are crucially important, change to logger.warning().
-    waccmOnly = [species for species in waccmSpecies if species not in musicaSpecies]
-    musicaOnly = [species for species in musicaSpecies if species not in waccmSpecies]
-    if (len(waccmOnly) > 0):
-        logger.info(f"The following chemical species are only in WACCM: {waccmOnly}")
-    if (len(musicaOnly) > 0):
-        logger.info(f"The following chemical species are only in MUSICA: {musicaOnly}")
-
-    # build the dictionary
-    # To do: As of September 4, 2024 this is not much of a map,
-    # as most of the entries are identical. We may map additional
-    # pairs in the future. This map is still useful in identifying
-    # the common species between WACCM and MUSICA.
-    if (modelType == fileUtils.WACCM_File):
-        varMap = {
-            # WACCM: MusicBox
-            "T": "temperature",
-            "lev": "pressure"       # sigma pressure coordinates
-        }
-    elif (modelType == fileUtils.WRF_Chem_File):
-        varMap = {
-            # WRF-Chem: MusicBox
-            "T2": "temperature",
-            "PB": "pressure"
-        }
-
-    for varName in inCommon:
-        varMap[varName] = varName
-
-    return (varMap)
 
 
 # Read array values at a single lat-lon-time point.
@@ -724,7 +666,7 @@ def main():
             musicaChems = getMusicaSpecies(templateFile)
 
             # create map of species common to both WACCM and MUSICA
-            commonDict = getMusicaDictionary(modelType, waccmChems, musicaChems)
+            commonDict = speciesMap.getMusicaDictionary(modelType, waccmChems, musicaChems)
             logger.info(f"Species in common are = {commonDict}")
             if (len(commonDict) == 0):
                 logger.warning("There are no common species between WACCM and your MUSICA species.json file.")
---
+++ python/acom_music_box/tools/speciesMap.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# speciesMap.py
+# MusicBox: Provide mapping of chemical species
+# from the atmospheric models (WACCM, WRF-Chem) to MusicBox.
+#
+# Author: Carl Drews
+# Copyright 2026 by Atmospheric Chemistry Observations & Modeling (UCAR/ACOM)
+
+from acom_music_box.tools import fileUtils
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+# Build and return dictionary of WACCM variable names
+# and their MusicBox equivalents.
+# waccmSpecies = list of variable names in the WACCM model output
+# musicaSpecies = list of variable names in species.json
+# return ordered dictionary
+def getMusicaDictionary(modelType, waccmSpecies=None, musicaSpecies=None):
+    if ((waccmSpecies is None) or (musicaSpecies is None)):
+        logger.warning("No species map found for WACCM or MUSICA.")
+
+        # build a simple species map
+        varMap = {
+            "T": "temperature",
+            "lev": "pressure",      # WACCM sigma pressure coordinates
+            "N2O": "N2O",
+            "H2O2": "H2O2",
+            "O3": "O3",
+            "NH3": "NH3",
+            "CH4": "CH4"
+        }
+
+        return (dict(sorted(varMap.items())))
+
+    # create new list of species common to both lists
+    inCommon = sorted([species for species in waccmSpecies if species in musicaSpecies])
+
+    # provide some diagnostic warnings
+    # If these messages are crucially important, change to logger.warning().
+    waccmOnly = [species for species in waccmSpecies if species not in musicaSpecies]
+    musicaOnly = [species for species in musicaSpecies if species not in waccmSpecies]
+    if (len(waccmOnly) > 0):
+        logger.info(f"The following chemical species are only in WACCM: {waccmOnly}")
+    if (len(musicaOnly) > 0):
+        logger.info(f"The following chemical species are only in MUSICA: {musicaOnly}")
+
+    # build the dictionary
+    # To do: As of September 4, 2024 this is not much of a map,
+    # as most of the entries are identical. We may map additional
+    # pairs in the future. This map is still useful in identifying
+    # the common species between WACCM and MUSICA.
+    if (modelType == fileUtils.WACCM_File):
+        varMap = {
+            # WACCM: MusicBox
+            "T": "temperature",
+            "lev": "pressure"       # sigma pressure coordinates
+        }
+    elif (modelType == fileUtils.WRF_Chem_File):
+        varMap = {
+            # WRF-Chem: MusicBox
+            "T2": "temperature",
+            "PB": "pressure"
+        }
+
+    for varName in inCommon:
+        varMap[varName] = varName
+
+    return (varMap)
+
